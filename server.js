edebc65e8055b44ed4e69a00ad921326db803dd9
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2023-02-14 23:01:59 +0530

    fixes

--- server.js
+++ server.js
@@ -17,36 +17,35 @@ function isBalanced(str) {
 	let lines = str.split(",");
 	console.log(lines);
 
-	for(let i = 0; i < lines.length; i++) {
-	for (let j = 0; j < lines[i].length; j++) {
-		let char = lines[i][j];
+	for (let i = 0; i < lines.length; i++) {
+		for (let j = 0; j < lines[i].length; j++) {
+			let char = lines[i][j];
 
-		if (char in map) {
-			stack.push(char);
-		} else if (char === ")" || char === "]" || char === "}") {
-			let last = stack.pop();
+			if (char in map) {
+				stack.push(char);
+			} else if (char === ")" || char === "]" || char === "}") {
+				let last = stack.pop();
 
-			if (char !== map[last]) {
-				return {
-					flag: -1,
-					line : i + 1,
-				};
+				if (char !== map[last]) {
+					return {
+						flag: -1,
+						line: i + 1,
+					};
+				}
 			}
 		}
 	}
-	}
 
-	if(stack.length  === 0){
+	if (stack.length === 0) {
 		return {
 			flag: 0,
-			line : null
-		}
-	}
-	else{
+			line: null,
+		};
+	} else {
 		return {
 			flag: 1,
-			line : lines.length
-		}
+			line: lines.length,
+		};
 	}
 }
 
@@ -62,45 +61,109 @@ const storage = multer.diskStorage({
 async function readJavaFile(filePath) {
 	const set = new Set();
 	const rl = readline.createInterface({
-	  input: fs.createReadStream(filePath),
-	  crlfDelay: Infinity
+		input: fs.createReadStream(filePath),
+		crlfDelay: Infinity,
 	});
-  
+
+	const reservedKeywords = [
+		"abstract",
+		"assert",
+		"boolean",
+		"break",
+		"byte",
+		"case",
+		"catch",
+		"char",
+		"class",
+		"const",
+		"continue",
+		"default",
+		"do",
+		"double",
+		"else",
+		"enum",
+		"extends",
+		"false",
+		"final",
+		"finally",
+		"float",
+		"for",
+		"if",
+		"implements",
+		"import",
+		"instanceof",
+		"int",
+		"interface",
+		"long",
+		"native",
+		"new",
+		"null",
+		"package",
+		"private",
+		"protected",
+		"public",
+		"return",
+		"short",
+		"static",
+		"strictfp",
+		"super",
+		"switch",
+		"synchronized",
+		"this",
+		"throw",
+		"throws",
+		"transient",
+		"true",
+		"try",
+		"void",
+		"volatile",
+		"while",
+	];
 	for await (const line of rl) {
-	  if (!line.trim().startsWith("//")  && !line.trim().startsWith("*")) {
-		line
-		  .split(/\W+/)
-		  .filter(word => word.length > 0)
-		  .forEach(word => set.add(word));
-	  }
+		if (!line.trim().startsWith("//") && !line.trim().startsWith("*")) {
+			line
+				.split(/\W+/)
+				.filter((word) => word.length > 0)
+				.forEach((word) => {
+					if (!reservedKeywords.includes(word)) {
+						set.add(word);
+					  }
+				});
+		}
 	}
 	return set;
 }
 
-async function readYamlFile(filePath){
-  const set = new Set();
-  const rl = readline.createInterface({
-    input: fs.createReadStream(filePath),
-    crlfDelay: Infinity
-  });
+async function readYamlFile(filePath) {
+	const set = new Set();
+	const rl = readline.createInterface({
+		input: fs.createReadStream(filePath),
+		crlfDelay: Infinity,
+	});
 
-  for await (const line of rl) {
-    // Split the line into words and add each word to the set
-    line
-      .split(/\W+/)
-      .filter(word => word.length > 0)
-      .forEach(word => set.add(word));
-  }
+	for await (const line of rl) {
+		// Split the line into words and add each word to the set
+		line
+			.split(/\W+/)
+			.filter((word) => word.length > 0)
+			.forEach((word) => set.add(word));
+	}
 
-  return set;
+	return set;
 }
-
 
 const uploaded = multer({ storage: storage });
 
 app.post("/", uploaded.array("files"), async function (req, res) {
 	try {
 		const file = req.files[1];
+		if (
+			req.files[0].originalname.includes(" ") ||
+			req.files[1].originalname.includes(" ")
+		) {
+			
+			res.status(404).json({ message: "Bad File Name" });
+		}
 		const data = fs.readFileSync(
 			__dirname + "/uploaded/" + file.originalname,
 			"utf8"
@@ -115,47 +178,52 @@ app.post("/", uploaded.array("files"), async function (req, res) {
 						ruleCode: individualElement.ruleCode,
 						status: "Valid Parenthesis",
 					});
-				} else if(resultObject.flag < 0) {
+				} else if (resultObject.flag < 0) {
 					results.push({
 						ruleCode: individualElement.ruleCode,
-						status: "Expected Opening Parenthesis before line " + resultObject.line,
+						status:
+							"Expected Opening Parenthesis before line " +
+							resultObject.line,
 					});
-				}
-				else{
+				} else {
 					results.push({
 						ruleCode: individualElement.ruleCode,
-						status: "Expected Closing Parenthesis in line " + resultObject.line,
+						status:
+							"Expected Closing Parenthesis in line " +
+							resultObject.line,
 					});
 				}
 			}
 		});
-		
-		const JavaFileWords = await readJavaFile(__dirname + "/uploaded/" + req.files[0].originalname);
 
-		const YamlFileWords = await readYamlFile(__dirname + "/uploaded/" + req.files[1].originalname);
-		
+		const JavaFileWords = await readJavaFile(
+			__dirname + "/uploaded/" + req.files[0].originalname
+		);
+
+		const YamlFileWords = await readYamlFile(
+			__dirname + "/uploaded/" + req.files[1].originalname
+		);
+
 		let foundWords = [];
 		let notFoundWords = [];
 
 		for (const element of JavaFileWords) {
 			if (YamlFileWords.has(element)) {
 				foundWords.push(element);
-			}
-			else{
+			} else {
 				notFoundWords.push(element);
 			}
 		}
 
-		res.status(200).json({ 
-			results : results,
-			foundWords : foundWords,
-			notFoundWords : notFoundWords
-		 });
+		res.status(200).json({
+			results: results,
+			foundWords: foundWords,
+			notFoundWords: notFoundWords,
+		});
 	} catch (error) {
 		res.status(500).json("Problem occurred while uploading");
 	}
 });
-
 
 app.listen(port, () => {
 	console.log("listening on port " + port);
